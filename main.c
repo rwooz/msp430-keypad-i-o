55ddffde56111071f72cb155886eb08db49d7e7d
rwooz <rywoo603@gmail.com>
2017-12-11 10:19:46 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
-//Ports:
-//1.0-1.3 = rows
-//2.0-2.2 = columns
-//1.4 = pump 1
-//1.5 = pump 2
-//2.3 = pump 3
-//2.4 = pump 4
-//Pumps run at ~100mL/min while, cycles run at ~1mil/sec
+/*
+Ports:
+1.0-1.3 = rows
+2.0-2.2 = columns
+1.4 = pump 1 rum
+1.5 = pump 2 vodka
+2.3 = pump 3 coke
+2.4 = pump 4 ginger beer
+Pumps run at ~100mL/min while, cycles run at ~1mil/sec
+*/
 #include <msp430g2553.h>
 #include <legacymsp430.h>
 #include <stdint.h>
@@ -27,14 +29,14 @@ int main(void)
 {
     // Disable watch dog timer
     WDTCTL = WDTPW+WDTHOLD;
-
-    // Set all pins as inputs
-    // pins 0, 1, 2, 3 of port 1 read values from the rows of the numpads
+	
+	// Set all pins as inputs
     P1DIR = 0;
     P2DIR = 0;
 
-    // Pins 0, 1, 2 of port 2 feed into the columns of the numpad
-    // pins 7, 6 of port 1 and pins 5 and 4 of port 2 feed the 4 LEDs
+	// pins 0, 1, 2, 3 of port 1 read values from the rows of the numpads
+	// Pins 0, 1, 2 of port 2 feed into the columns of the numpad
+    // pins 4,5 of port 1 and pins 3,4 of port 2 feed the different pumps
     P1DIR |= (BIT0 | BIT1 | BIT2 | BIT3 | BIT4 | BIT5);
     P2DIR |= (BIT3 | BIT4 | BIT5);
 
@@ -46,17 +48,17 @@ int main(void)
 
     uint32_t io_pins[4] = {BIT0, BIT1, BIT2, BIT3};
 
-    uint32_t keypress1 = 0xC, keypress2 = 0xC; //initialize keypress to value not on keypad
+    uint32_t keypress1 = 0xC, keypress2 = 0xC; //initialize keypresses to values not found on keypad (null/0 is on keypad [could also just change the keymap_hex values, but would need this line anyways to initialize])
 
     while(1) {
         for(row = 0; row < 4; row++)
         {
-            P1OUT |= io_pins[row];
+            P1OUT |= io_pins[row]; //while on row, set to high
             for(col = 0; col < 3; col++)
             {
-                if(P2IN & io_pins[col])
+                if(P2IN & io_pins[col]) //if the row and the column are high, there is a match
                 {
-                    //led_output(keymap_hex[row*3+col]);
+                    //led_output(keymap_hex[row*3+col]); //led debug
                     if(keypress2 == 0xC)                //detect if first keypress or 2nd keypress
                     {
                         if(keypress1 == 0xC)
@@ -75,35 +77,51 @@ int main(void)
                     }
                     if(keymap_hex[row*3+col] == 0xB)        //if user presses #, executes then clears
                     {
-                        if(keypress1 == 0xA && keypress2 == 0x0)        //recipe for 01
+                        if(keypress1 == 0xA && keypress2 == 0x0)        //recipe for 0-1 (rum and coke 2:5)
                         {
-                            P2OUT |= (BIT3 | BIT4);
-                            __delay_cycles(5000000);
-                            P2OUT &= ~(BIT3 | BIT4);
                             P1OUT |= BIT4;
-                            __delay_cycles(5000000);
+							P2OUT |= BIT3;
+                            __delay_cycles(36000000);
                             P1OUT &= ~BIT4;
+							__delay_cycles(50000000);
+							P2OUT &= ~BIT3;
                         }
-                        if(keypress1 == 0xA && keypress2 == 0x1)        //recipe for 02
+                        if(keypress1 == 0xA && keypress2 == 0x1)        //recipe for 0-2 (dark and stormy 2:5)
                         {
-                            P2OUT |= BIT3;
-                            __delay_cycles(5000000);
-                            P2OUT &= ~BIT3;
+                            P1OUT |= BIT4;
+							P2OUT |= BIT4;
+                            __delay_cycles(36000000);
+                            P1OUT &= ~BIT4;
+							__delay_cycles(50000000);
+							P2OUT &= ~BIT4;
                         }
-                        if(keypress1 == 0xA && keypress2 == 0x2)        //recipe for 03
+                        if(keypress1 == 0xA && keypress2 == 0x2)        //recipe for 0-3 (moscow mule 2:5)
                         {
-                            P2OUT |= BIT3;
+                            P1OUT |= BIT5;
+							P2OUT |= BIT4;
+							__delay_cycles(36000000);
+							P1OUT &= ~BIT5;
+							__delay_cycles(50000000);
+							P2OUT &= ~BIT4;
                         }
+						if(keypress1 == 0xA && keypress2 == 0x3)		//recipe for 0-4 (something probably gross 1:1:1:1)
+						{
+							P1OUT |= (BIT4 | BIT5);
+							P2OUT |= (BIT3 | BIT4);
+							__delay_cycles(30000000);
+							P1OUT &= ~(BIT4 | BIT5);
+							P2OUT &= ~(BIT3 | BIT4);
+						}
                         keypress1 = 0xC;
                         keypress2 = 0xC;
                     }
                 }
             }
-            P1OUT &= ~(BIT0 | BIT1 | BIT2 | BIT3);
+            P1OUT &= ~(BIT0 | BIT1 | BIT2 | BIT3); //reset values
         }
     }
 }
-/*
+/* LED output function used for debugging purposes, outputs binary values of keypad to 4 leds
 void led_output(uint8_t bits)
 {
     (bits & BIT0) ? (P1OUT |= BIT7) : (P1OUT &= ~BIT7);
